d79012617d81239b2bb2ce8a29fb93c7581e519a
Mrunal Sunil Dhobe <37969422+mdhobe@users.noreply.github.com>
2022-08-24 08:41:37 +0530

    https://youtu.be/PJdOUDWYstY?list=PLDdcY4olLQk3cAxZPJXMbxqrM3PlNkmO8

--- GERGOVIA -Wine trading in Gergovia.java
+++ GERGOVIA -Wine trading in Gergovia.java
@@ -57,3 +57,56 @@
 // }
 
 //using extra space
+import java.io.*;
+import java.util.*;
+
+public class C {
+
+	public void main() {
+		try {
+			System.setIn(new FileInputStream("input.txt"));
+			System.setOut(new PrintStream(new FileOutputStream("output.txt")));
+		} catch (Exception e) {
+			System.err.println("Error");
+		}
+
+		Scanner scan = new Scanner(System.in);
+		int[] a = { 5, -4, 1, -3, 1};
+
+		gergovia(a);
+	}
+
+	public void gergovia(int[] nums) {
+		int buyerp = 0, sellerp = 0, ans = 0;
+
+		ArrayList<ArrayList<Integer>> buyers = new ArrayList<>();
+		ArrayList<ArrayList<Integer>> sellers = new ArrayList<>();
+
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] > 0) {
+				ArrayList<Integer> item = new ArrayList<>();
+				item.add(nums[i]);
+				item.add(i);
+				buyers.add(item);
+			} else if (nums[i] < 0) {
+				ArrayList<Integer> item = new ArrayList<>();
+				item.add(nums[i]);
+				item.add(i);
+				sellers.add(item);
+			}
+		}
+
+		while (buyerp < buyers.size() && sellerp < sellers.size()) {
+			int min = Math.min(buyers.get(buyerp).get(0), -1 * sellers.get(sellerp).get(0));
+			buyers.get(buyerp).set(0, buyers.get(buyerp).get(0) - min);
+			sellers.get(sellerp).set(0, sellers.get(sellerp).get(0) + min);
+			int diff = Math.abs(buyers.get(buyerp).get(1) - sellers.get(sellerp).get(1));
+			ans += diff * min;
+			if (buyers.get(buyerp).get(0) == 0) buyerp++;
+			if (sellers.get(sellerp).get(0) == 0) sellerp++;
+		}
+
+		System.out.println(ans);
+	}
+
+}

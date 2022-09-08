2c6f642e7ed9dbc6dd8275a9cb1c64767fb4c95d
Mrunal Sunil Dhobe <37969422+mdhobe@users.noreply.github.com>
2022-09-08 13:54:29 +0530

    https://leetcode.com/problems/implement-queue-using-stacks/

--- Implement Queue using Stacks.java
+++ Implement Queue using Stacks.java
@@ -1,40 +1,63 @@
+// Using 2 Stacks
+// class MyQueue {
+//     Stack<Integer> s1;
+//     Stack<Integer> s2;
+
+//     public MyQueue() {
+//         s1 = new Stack<Integer>();
+//         s2 = new Stack<Integer>();
+//     }
+    
+//     public void push(int x) {
+//         while(!s1.isEmpty()) s2.push(s1.pop());
+//         s1.push(x);
+//         while(!s2.isEmpty()) s1.push(s2.pop());
+//     }
+    
+//     public int pop() {
+//         return s1.pop();
+//     }
+    
+//     public int peek() {
+//         return s1.peek();
+//     }
+    
+//     public boolean empty() {
+//         return s1.isEmpty();
+//     }
+// }
+
+// Can you implement the queue such that each operation is amortized O(1) time complexity? In other words, performing n operations will take overall O(n) time even if one of those operations may take longer.
+
 class MyQueue {
-    Stack<Integer> s1;
-    Stack<Integer> s2;
+    Stack<Integer> input;
+    Stack<Integer> output;
 
     public MyQueue() {
-        s1 = new Stack<Integer>();
-        s2 = new Stack<Integer>();
+        input = new Stack<Integer>();
+        output = new Stack<Integer>();
     }
     
     public void push(int x) {
-        s1.add(x);
+        input.add(x);
     }
     
     public int pop() {
-        while(!s1.isEmpty()) s2.push(s1.pop());
-        int a = s2.pop();
-        while(!s2.isEmpty()) s1.push(s2.pop());
-        return a;
+        if(output.isEmpty()){
+            while(!input.isEmpty()) output.push(input.pop());
+        }
+        return output.pop();
     }
     
     public int peek() {
-        while(!s1.isEmpty()) s2.push(s1.pop());
-        int a = s2.peek();
-        while(!s2.isEmpty()) s1.push(s2.pop());
-        return a;
+        if(output.isEmpty()){
+            while(!input.isEmpty()) output.push(input.pop());
+        }
+        return output.peek();
     }
     
     public boolean empty() {
-        return s1.isEmpty();
+        if(input.isEmpty() && output.isEmpty()) return true;
+        return false;
     }
 }
-
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue obj = new MyQueue();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.peek();
- * boolean param_4 = obj.empty();
- */

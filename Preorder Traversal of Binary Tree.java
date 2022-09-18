f3f7ff9096cd4e90da77119566a4e63568993533
Mrunal Sunil Dhobe <37969422+mdhobe@users.noreply.github.com>
2022-09-18 18:41:06 +0530

    https://youtu.be/RlUu72JrOCQ?list=PLgUwDviBIf0q8Hkd7bK2Bpryj2xVJk8Vk

--- Preorder Traversal of Binary Tree.java
+++ Preorder Traversal of Binary Tree.java
@@ -1,43 +1,29 @@
-import java.io.*;
-import java.util.*;
-
-public class C {
-
-	public void main() {
-		try {
-			System.setIn(new FileInputStream("input.txt"));
-			System.setOut(new PrintStream(new FileOutputStream("output.txt")));
-		} catch (Exception e) {
-			System.err.println("Error");
-		}
-
-		Scanner scan = new Scanner(System.in);
-		Node node = new Node(1);
-		createNodeOrder(node);
-		preOrder(node);
-	}
-
-	public void createNodeOrder(Node node) {
-		node.left = new Node(2);
-		node.right = new Node(3);
-		node.left.left = new Node(4);
-		node.left.right = new Node(5);
-		node.right.left = new Node(6);
-		node.right.right = new Node(7);
-	}
-
-	public void preOrder(Node node) {
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<Integer>();
+        preOrder(root, ans);
+        return ans;
+    }
+    
+    public void preOrder(TreeNode node, List<Integer> ans) {
 		if (node == null) return;
-		System.out.println(node.val);
-		preOrder(node.left);
-		preOrder(node.right);
-	}
-
-	class Node {
-		int val;
-		Node left, right;
-		Node(int data) {
-			val = data;
-		}
+		ans.add(node.val);
+		preOrder(node.left, ans);
+		preOrder(node.right, ans);
 	}
 }

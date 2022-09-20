ea7e7bf68d48d02b860b780499fa950ec63d25bc
Mrunal Sunil Dhobe <37969422+mdhobe@users.noreply.github.com>
2022-09-20 23:07:59 +0530

    https://www.youtube.com/watch?v=Yt50Jfbd8Po&list=PLgUwDviBIf0q8Hkd7bK2Bpryj2xVJk8Vk&index=16

---
+++ Check for Balanced Binary Tree.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return dfsHeight(root) != -1;
+    }
+    public int dfsHeight(TreeNode root){
+        if(root == null) return 0;
+        int left = dfsHeight(root.left);
+        if(left == -1) return -1;
+        int right = dfsHeight(root.right);
+        if(right == -1) return -1;
+        
+        if(Math.abs(left-right) > 1) return -1;
+        return 1 + Math.max(left, right);
+    }
+}

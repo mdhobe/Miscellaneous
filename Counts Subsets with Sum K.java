956029fcfecc904b49b6d0a93b0947154b476b78
Mrunal Sunil Dhobe <37969422+mdhobe@users.noreply.github.com>
2023-02-24 23:17:35 +0530

    https://www.youtube.com/watch?v=ZHyb-A2Mte4&list=PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY&index=18

---
+++ Counts Subsets with Sum K.java
@@ -0,0 +1,77 @@
+//Memoization
+public class Solution {
+    public static int findWays(int num[], int tar) {
+        // Write your code here..
+        int n = num.length;
+        int[][] dp = new int[n][tar+1];
+        for(int i=0;i<n;i++) for(int j=0;j<=tar;j++) dp[i][j] = -1;
+        
+        return findWays(n-1, num, tar, dp);
+    }
+
+    public static int findWays(int current,int num[], int target, int[][] dp){
+        if(target == 0) return 1;
+        if(current == 0) return (num[current] == target) == true ? 1 : 0;
+        if(dp[current][target] != -1) return dp[current][target];
+
+        int pick = 0;
+        if(num[current] <= target) pick = findWays(current-1, num, target - num[current],dp);
+        int notpick = findWays(current-1, num, target,dp);
+
+        return dp[current][target] = pick + notpick;
+    }
+}
+
+//Tabulation
+public class Solution {
+    public static int findWays(int num[], int tar) {
+        // Write your code here..
+        int n = num.length;
+        int[][] dp = new int[n][tar+1];
+
+        //target = 0
+        for(int i=0;i<n;i++) dp[i][0] = 1;
+        if(num[0] <= tar) dp[0][num[0]] = 1;
+
+        //compute
+        for(int i=1;i<n;i++){
+            for(int t=0;t<=tar;t++){
+                int notpick = dp[i-1][t];
+                int pick = 0;
+                if(num[i] <= t) pick = dp[i-1][t-num[i]];
+                dp[i][t] = pick + notpick;
+            }
+        }
+
+        return dp[n-1][tar];
+
+    }
+}
+
+//Space Optimization
+public class Solution {
+    public static int findWays(int num[], int tar) {
+        // Write your code here..
+        int n = num.length;
+        int[] dp = new int[tar+1];
+
+        //target = 0
+        dp[0] = 1;
+        if(num[0] <= tar) dp[num[0]] = 1;
+
+        //compute
+        for(int i=1;i<n;i++){
+            int[] temp = new int[tar+1];
+            for(int t=0;t<=tar;t++){
+                int notpick = dp[t];
+                int pick = 0;
+                if(num[i] <= t) pick = dp[t-num[i]];
+                temp[t] = pick + notpick;
+            }
+            for(int t=0;t<=tar;t++) dp[t] = temp[t];
+        }
+
+        return dp[tar];
+
+    }
+}
